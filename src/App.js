c0a0fa92d8c7cde361f057e1aedb93016538033c
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-09 11:01:24 +0200

    toggler is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const togglerHandler = () => {
+    setShowParagraph((prevhowParagraph) => !prevhowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is New !!!</p>}
+
+      <Button onClick={togglerHandler}>Toggle</Button>
     </div>
   );
 }
 
 export default App;
+
+
+
